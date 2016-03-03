3be808a02e7edc310f7504f14a87b413d41fd4a3
PKuhn <patrick.kuhn@student.hpi.de>
2016-03-03 23:41:10 +0100

    Included named entity recoqnizer

--- relation_extractor.py
+++ relation_extractor.py
@@ -1,4 +1,4 @@
-# Set up spaCy
+import nltk
 from spacy.en import English
 
 
@@ -11,10 +11,35 @@ def build_parse_tree():
     root = find_root(dependency_tree)
     print(root)
 
+
 def find_root(dependency_tree):
     for token in dependency_tree:
         if token.head == token:
             return token
 
+
+def find_named_entities(sent):
+    tokens = nltk.word_tokenize(sent)
+    tokens = nltk.pos_tag(tokens)
+    tree = nltk.ne_chunk(tokens)
+    named_entities = []
+    for child in tree:
+        named_entities.append(extract_entity_names(child))
+    return named_entities
+
+
+def extract_entity_names(t):
+    entity_names = []
+    if hasattr(t, 'label') and t.label:
+        print(t.label())
+        if t.label() == 'NE':
+            entity_names.append(' '.join([child[0] for child in t]))
+        else:
+            for child in t:
+                entity_names.extend(extract_entity_names(child))
+
+    return entity_names
+
 if __name__ == "__main__":
-    build_parse_tree()
+    sent = "I am very excited about the next generation of Apple products."
+    find_named_entities(sent)
